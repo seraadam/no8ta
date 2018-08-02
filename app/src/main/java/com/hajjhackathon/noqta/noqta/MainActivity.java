e61fe880db617b7f18f8a1a4d54600ba789655e9
Esraa Ahmed <esraaahmed@Esraas-MacBook-Air-2.local>
2018-08-02 03:39:22 +0300

    worked app but the textblock need to be changed with the line

--- app/src/main/java/com/hajjhackathon/noqta/noqta/MainActivity.java
+++ app/src/main/java/com/hajjhackathon/noqta/noqta/MainActivity.java
@@ -1,35 +1,74 @@
 package com.hajjhackathon.noqta.noqta;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.WindowDecorActionBar;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     Context mcontext;
     CameraSource visioncam;
+    SurfaceView CameraView;
+    TextView errortext;
 
+public static final int requestPermissionID = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mcontext = getApplication();
 
+        CameraView = (SurfaceView) findViewById(R.id.surfaceView);
+        errortext= (TextView) findViewById(R.id.error);
 
-        if (checkCameraHardware(mcontext)) {
+       // if (checkCameraHardware(mcontext)) {
             ImageDetection(true);
 
+     //   }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != requestPermissionID) {
+            Log.d("permission result", "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
         }
 
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            try {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                visioncam.start(CameraView.getHolder());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     //check if the device if it has a camera
@@ -63,12 +102,76 @@ public class MainActivity extends AppCompatActivity {
             //3)define the camera source to make realtime view for the text recognizer
             visioncam = new CameraSource.Builder(mcontext, textrecognizer)
                     .setFacing(CameraSource.CAMERA_FACING_BACK)
-                    .setRequestedPreviewSize(getWindow().getDecorView().getWidth(), getWindow().getDecorView().getHeight())
-                    .setAutoFocusEnabled(focus).build();
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(focus).setRequestedFps(2.0f).build();
 //get the value after vision cloud responce
 
+
+            /**
+             * Add call back to SurfaceView and check if camera permission is granted.
+             * If permission is granted we can start our cameraSource and pass it to surfaceView
+             */
+            CameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    try {
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    requestPermissionID);
+                            return;
+                        }
+                        visioncam.start(CameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    visioncam.stop();
+                }
+            });
+
+            //Set the TextRecognizer's Processor.
+            textrecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+                }
+
+                /**
+                 * Detect all the text from camera using TextBlock and the values into a stringBuilder
+                 * which will then be set to the textView.
+                 */
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if (items.size() != 0) {
+
+                        errortext.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for (int i = 0; i < items.size(); i++) {
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                errortext.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
         }
-
-
     }
 }
+
+
